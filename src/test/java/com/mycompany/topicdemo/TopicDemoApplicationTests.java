dfd845e37d87951051840cd00cfa5abea8a089e1
ilmars.kozlovs <ilmars.kozlovs@accenture.com>
2019-06-10 11:58:09 +0300

    test commit

--- src/test/java/com/mycompany/topicdemo/TopicDemoApplicationTests.java
+++ src/test/java/com/mycompany/topicdemo/TopicDemoApplicationTests.java
@@ -3,11 +3,19 @@ package com.mycompany.topicdemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class TopicDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void simpleTest(){
+		String name = "Janis";
+		assertThat("Janis").isEqualTo(name);
 	}
 
 }
