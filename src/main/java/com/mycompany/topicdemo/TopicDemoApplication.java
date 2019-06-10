4d40336bbd2ae4ebe4a41aab17f9b2af042acc06
ilmars.kozlovs <ilmars.kozlovs@accenture.com>
2019-06-10 11:53:38 +0300

    Initial commit

---
+++ src/main/java/com/mycompany/topicdemo/TopicDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.topicdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/topicdemo/TopicDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.topicdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
