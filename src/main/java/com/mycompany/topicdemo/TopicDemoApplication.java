072a359774cfe6583c85305e64e862b640835166
ilmars.kozlovs <ilmars.kozlovs@accenture.com>
2019-06-10 14:42:07 +0300

    topic findAll functionality added

--- src/main/java/com/mycompany/topicdemo/TopicDemoApplication.java
+++ src/main/java/com/mycompany/topicdemo/TopicDemoApplication.java
@@ -1,7 +1,13 @@
 package com.mycompany.topicdemo;
 
+import com.mycompany.topicdemo.model.Topic;
+import com.mycompany.topicdemo.repository.TopicRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TopicDemoApplication {
@@ -10,4 +16,18 @@ public class TopicDemoApplication {
 		SpringApplication.run(TopicDemoApplication.class, args);
 	}
 
+	private static final Logger log = LoggerFactory.getLogger(TopicDemoApplication.class);
+
+	@Bean
+	public CommandLineRunner saveDefaultTopics(TopicRepository topicRepository){
+		return (args -> {
+			topicRepository.save(new Topic("java", "oracle.com", "topic for java"));
+			topicRepository.save(new Topic("python", "python.com", "learning site for python"));
+			topicRepository.save(new Topic("php", "php.de", "php website"));
+
+			topicRepository.findAll().forEach(c -> {
+				log.info("Topic: {}", c);
+			});
+		});
+		}
 }
