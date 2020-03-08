4a487646229e96726f6d16e09c6110b062b70908
Ilmars <ilmars.kozlovs@inbox.lv>
2020-03-08 09:07:39 +0200

    newbranch commit 2

--- src/main/java/com/mycompany/topicdemo/TopicDemoApplication.java
+++ src/main/java/com/mycompany/topicdemo/TopicDemoApplication.java
@@ -26,6 +26,9 @@ public class TopicDemoApplication {
 			topicRepository.save(new Topic("php", "php.de", "php website"));
 			topicRepository.save(new Topic("php1", "php.de", "php website"));
 			topicRepository.save(new Topic("php2", "php.de", "php website"));
+			topicRepository.save(new Topic("php3", "php.de", "php website"));
+			topicRepository.save(new Topic("php4", "php.de", "php website"));
+			topicRepository.save(new Topic("php5", "php.de", "php website"));
 
 			topicRepository.findAll().forEach(c -> {
 				log.info("Topic: {}", c);
